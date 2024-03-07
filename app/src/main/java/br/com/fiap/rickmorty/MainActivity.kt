dcaaa63c11b476dac0a91f5085a8159b447af620
Prof Celso <celso@celso.dev.br>
2024-03-07 17:21:25 -0300

    Tela concluída

--- app/src/main/java/br/com/fiap/rickmorty/MainActivity.kt
+++ app/src/main/java/br/com/fiap/rickmorty/MainActivity.kt
@@ -3,14 +3,46 @@ package br.com.fiap.rickmorty
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.fiap.rickmorty.ui.theme.RickMortyTheme
+import coil.compose.AsyncImage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +51,14 @@ class MainActivity : ComponentActivity() {
             RickMortyTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    Greeting("Android")
+                    Image(
+                        painter = painterResource(id = R.drawable.rm_wallpaper),
+                        contentDescription = "",
+                        contentScale = ContentScale.Crop
+                    )
+                    Tela()
                 }
             }
         }
@@ -30,17 +66,104 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Tela() {
+    Column(
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Exibir personagens")
+        }
+        LazyColumn() {
+            items(10) {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(150.dp)
+                        .padding(top = 8.dp),
+                    colors = CardDefaults
+                        .cardColors(
+                            containerColor = Color(0x55673AB7)
+                        ),
+                    border = BorderStroke(
+                        width = 1.dp,
+                        Color.White
+                    )
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .fillMaxSize(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Card(
+                            modifier = Modifier
+                                .size(
+                                    width = 80.dp,
+                                    height = 80.dp
+                                ),
+                            shape = CircleShape
+                        ) {
+                            AsyncImage(
+                                model = "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+                                contentDescription = ""
+                            )
+                        }
+                        Column(
+                            verticalArrangement = Arrangement.Center,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(
+                                    vertical = 16.dp,
+                                    horizontal = 16.dp
+                                )
+                        ) {
+                            Text(
+                                text = "Rick Sanchez",
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.White
+                            )
+                            Text(
+                                text = "Alive",
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.White
+                            )
+                            Text(
+                                text = "Human - Male",
+                                fontSize = 12.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.White
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
 @Composable
-fun GreetingPreview() {
+fun TelaPreview() {
     RickMortyTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.rm_wallpaper),
+                contentDescription = "",
+                contentScale = ContentScale.Crop
+            )
+            Tela()
+        }
     }
 }
